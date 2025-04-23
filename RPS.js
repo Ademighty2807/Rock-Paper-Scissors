ad83e7594fe74cb990b97344bc1645bc9d8ad8ef
Ademighty2807 <^[[200~202931993+Ademighty2807@users.noreply.github.com>
2025-04-23 10:43:12 +0100

    rock paper scissors game javascript exercise

---
+++ RPS.js
@@ -0,0 +1,77 @@
+function rock () {
+    const randomNumber = Math.random();
+      let computerScore = '';
+
+      if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerScore = 'rock';
+          } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+            computerScore = 'paper';
+          } else if ( randomNumber >= 2 / 3 && randomNumber < 1) {
+            computerScore = 'Scissors';
+          }
+
+
+          let result = '';
+
+          if (computerScore === 'rock') {
+            result= 'Tie.'
+          } else if (computerScore === 'paper') {
+            result = 'You lose.';
+          } else if (computerScore === 'Scissors') {
+            result = 'You win.';
+          }
+
+          alert(`You picked rock. Computer picked. ${computerScore}. ${result}`)
+}
+
+function paper () {
+    const randomNumber = Math.random();
+        let computerScore = '';
+
+        if (randomNumber >= 0 && randomNumber < 1 / 3) {
+            computerScore = 'rock';
+            } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+                computerScore = 'paper';
+            } else if ( randomNumber >= 2 / 3 && randomNumber < 1) {
+                computerScore = 'Scissors';
+            }
+
+
+            let result = '';
+
+            if (computerScore === 'rock') {
+                result= 'You win.';
+            } else if (computerScore === 'paper') {
+                result = 'Tie.';
+            } else if (computerScore === 'Scissors') {
+                result = 'You lose.';
+            }
+
+            alert(`You picked paper. Computer picked. ${computerScore}. ${result}`)
+}
+
+function scissors () {
+    const randomNumber = Math.random();
+      let computerScore = '';
+
+      if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerScore = 'rock';
+          } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+            computerScore = 'paper';
+          } else if ( randomNumber >= 2 / 3 && randomNumber < 1) {
+            computerScore = 'Scissors';
+          }
+
+
+          let result = '';
+
+          if (computerScore === 'rock') {
+            result= 'You lose.';
+          } else if (computerScore === 'paper') {
+            result = 'You win.';
+          } else if (computerScore === 'Scissors') {
+            result = 'Tie.';
+          }
+
+          alert(`You picked Scissors. Computer picked. ${computerScore}. ${result}`)
+}
